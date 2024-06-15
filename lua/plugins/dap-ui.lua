388cb727f5bdddc53aebfcf4b79d0603b66e891e
huhinka <huhinka@163.com>
2024-06-15 12:28:52 +0800

    override dap-ui default settings to prevent automatic opening and closing of windows

---
+++ lua/plugins/dap-ui.lua
@@ -0,0 +1,10 @@
+return {
+  "rcarriga/nvim-dap-ui",
+  config = function()
+    -- Override the default settings to prevent automatic opening and closing of windows,
+    -- as this can be noisy when debugging with a watchdog.
+    local dapui = require("dapui")
+    dapui.setup()
+    return {}
+  end,
+}
