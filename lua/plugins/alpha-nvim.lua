bbdfc12e98a92a16c53529f7d803ffd1f3cf52af
huhinka <huhinka@163.com>
2024-07-03 18:08:49 +0800

    add project button to dashboard

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -12,6 +12,17 @@ return {
       "░█─░█ ░█─░█ ░█─░█ ▀──▀ ─ ▀▀▀ 　 ░█▄▄▄█ ──▀── ─▀▀▀ ▀▀▀─ ▀▀▀ ▀▀▀▀     ▀▄▄▄▄▄▀▀ ",
     }
     opts.section.header.val = logo
+
+    local get_icon = require("astroui").get_icon
+    opts.section.buttons.val = {
+      opts.button("LDR P  ", get_icon("FolderClosed", 2, true) .. "Open Project  "),
+      opts.button("LDR n  ", get_icon("FileNew", 2, true) .. "New File  "),
+      opts.button("LDR f f", get_icon("Search", 2, true) .. "Find File  "),
+      opts.button("LDR f o", get_icon("DefaultFile", 2, true) .. "Recents  "),
+      opts.button("LDR f w", get_icon("WordFile", 2, true) .. "Find Word  "),
+      opts.button("LDR f '", get_icon("Bookmarks", 2, true) .. "Bookmarks  "),
+      opts.button("LDR S l", get_icon("Refresh", 2, true) .. "Last Session  "),
+    }
     return opts
   end,
 }
